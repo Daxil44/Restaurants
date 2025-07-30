a5a40b4dcc02b50ca35022ebebf0436e458cc285
Daxil Patel <Daxil.Patel@catalisgov.com>
2025-07-30 09:42:07 -0600

    Seeding

---
+++ Restaurants.Infrastructure/Seeders/RestaurantSeeder.cs
@@ -0,0 +1,8 @@
+﻿using Restaurants.Infrastructure.Persistence;
+
+namespace Restaurants.Infrastructure.Seeders
+{
+    internal class RestaurantSeeder(RestaurantsDbContext dbContext)
+    {
+    }
+}
