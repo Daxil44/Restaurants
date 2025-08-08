22e2cb9f9bbac7f74499d81df90d056b807ead8c
Daxil Patel <Daxil.Patel@catalisgov.com>
2025-08-08 08:09:47 -0600

    Retrieving entities

--- Restaurants.API/Program.cs
+++ Restaurants.API/Program.cs
@@ -1,5 +1,7 @@
+using Restaurants.Application.Extensions;
 using Restaurants.Infrastructure.Extensions;
 using Restaurants.Infrastructure.Seeders;
+using Restaurants.Application.Extensions;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -9,6 +11,7 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddApplication();
 
 builder.Services.AddInfrastructure(builder.Configuration);
 
---
+++ Restaurants.Infrastructure/Repositories/RestaurantsRepository.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Restaurants.Domain.Entities;
+using Restaurants.Domain.Repositories;
+using Restaurants.Infrastructure.Persistence;
+
+internal class RestaurantsRepository(RestaurantsDbContext dbContext)
+    : IRestaurantsRepository
+{
+    public async Task<IEnumerable<Restaurant>> GetAllAsync()
+    {
+        var restaurants = await dbContext.Restaurants.ToListAsync();
+        return restaurants;
+    }
+}
